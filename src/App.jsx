5808ba9ba73d5f515ee4a95b80ba0923a6422954
KanishaCarter <kanishacarter90@outlook.com>
2025-07-01 15:36:34 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+/* Import array from data.js // Render list of episode names
+// UseState selected episode from list
+// Each item has unique key
+// Message tells user to select episode if none is selected
+// Episode's details are given once selected (Number, Name, Description)
+// UI elements are organized as component functions
+// Style with CSS
+*/
+
 export default function App() {
   // TODO
 }
